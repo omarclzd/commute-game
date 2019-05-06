ba2c912d8d7266f2d7820154c507b2851d7afae9
omar <omasrt@gmail.com>
2019-05-06 16:49:43 -0500

    added story content

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 /*----- constants -----*/ 
 
 const story = {
-  'Home': 'You are at home.',
-  'Floor': 'The floor is dirty.',
-  'Kitchen': 'Make breakfast',
-  'Window': 'Its raining outside.'
+  'standby': 'Get to work!',
+  'Home': 'Alarm rings.',
+  'Wakeup': 'You are away, but theres a funky smell.',
+  'Snoose': 'Promt for snoose',
+  'Brakealarm': 'You smash your alarm in defiance and now your hand is bleeding furiously.',
+  'Shower': 'While singing in the shower you atempt a power glide and break your neck in the process.',
+  'Kitchen': 'Kitchen promt',
 
   
 };
 
 const storyChoice = {
-  'Home': {'button-one': 'Look at window.', 'button-two': 'Go to kitchen', 'button-three': 'look at floor'},
+  'Home': {'button-one': 'Wake up', 'button-two': 'Snoose', 'button-three': 'Brake alarm'},
 
-  'Floor': {'button-one': 'Go home', 'button-two': 'Go to kitchen','button-three': 'Look at window.'},
+  'Wakeup': {'button-one': 'take cold shower', 'button-two': 'Skip shower and make breakfast','button-three': 'Choice 3.'},
 
-  'Kitchen': {'button-one': 'Go home', 'button-two': 'Look at window', 'button-three': 'look at floor'},
+  'Snoose': {'button-one': 'Choice1', 'button-two': 'Choice2', 'button-three': 'Choice3'},
 
-  'Window': {'button-one': 'Go home', 'button-two': 'look at floor', 'button-three': 'Go to kitchen'},
+  'Brakealarm': {'button-one': 'tend wound', 'button-two': 'lick wound', 'button-three': 'ignore'},
+
+  'Kitchen': {'button-one': 'Kchoice1', 'button-two': 'Kchoice2', 'button-three': 'Kchoice3'},
+
+  'death': {'button-one': 'You', 'button-two': 'Have', 'button-three': 'Died'},
 
 };
 
@@ -36,36 +43,37 @@ const button3 = document.getElementById('button-three');
 document.getElementById('button-one').addEventListener('click', handleBOne);
 document.getElementById('button-two').addEventListener('click', handleBTwo);
 document.getElementById('button-three').addEventListener('click', handleBThree);
-
 document.querySelector('button').addEventListener('click', handleStart);
 
 /*----- functions -----*/
 init();
 
 function handleStart(evt) {
-  if (state !== story['Home']) {
+  if (state === story['standby']) {
     return state = story['Home'];
-  }
-
-render();
+  } 
+  render();
 }
 
 function handleBOne(evt) {
-  if (state !== story['Window']) {
-    return state = story['Window'];
+  if (state === story['Home']) {
+    return state = story['Wakeup'];
   }
-
   render();
 }
 
 function handleBTwo(evt) {
- 
+  if (state === story['Home']) {
+    return state = story['Snoose'];
+  }
+  render();
 }
 
 function handleBThree(evt) {
-  let choice = evt.target;
-
-
+  if (state === story['Home']) {
+    return state = story['Brakealarm'];
+  }
+  render();
 }
 
 
@@ -77,12 +85,24 @@ function render() {
     button3.textContent = storyChoice['Home']['button-three'];
 
   } else {
-    if (state === story['Window']) {
-    button1.textContent = storyChoice['Window']['button-one'];
-    button2.textContent = storyChoice['Window']['button-two'];
-    button3.textContent = storyChoice['Window']['button-three'];
-    }
-  }
+    if (state === story['Wakeup']) {
+    button1.textContent = storyChoice['Wakeup']['button-one'];
+    button2.textContent = storyChoice['Wakeup']['button-two'];
+    button3.textContent = storyChoice['Wakeup']['button-three'];
+    } else {
+      if (state === story['Snoose']) {
+      button1.textContent = storyChoice['Snoose']['button-one'];
+      button2.textContent = storyChoice['Snoose']['button-two'];
+      button3.textContent = storyChoice['Snoose']['button-three'];
+      } else {
+        if (state === story['Brakealarm']) {
+          button1.textContent = storyChoice['Brakealarm']['button-one'];
+          button2.textContent = storyChoice['Brakealarm']['button-two'];
+          button3.textContent = storyChoice['Brakealarm']['button-three'];
+        }
+      }
+    } 
+  } 
  
   
 
@@ -92,7 +112,7 @@ function render() {
 }
 
 function init() {
-  state = story[''];
+  state = story['standby'];
   render();
 
 }
