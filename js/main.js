814907fe0d3f62c2b9258012604b95ff369c764a
omar <omasrt@gmail.com>
2019-05-07 17:15:49 -0500

    added grid to css

--- js/main.js
+++ js/main.js
@@ -2,18 +2,33 @@
 
 const story = {
   standby: 'Get to work!',
+
   home: 'Alarm rings.',
+
   wakeup: 'You are awake, but there is a funky smell.',
+
   brakeAlarm: 'You smash your alarm in defiance and now your hand is bleeding furiously.',
+
   tendWound: 'You did not pay attention in first aid class. You die from blood loss.',
+
   lickWound: 'You realize what you have been missing, and set off to be like your hero Edward Cullen. However, you did not make it to work.',
+
   shower: 'While singing in the shower you atempt a power glide and break your neck in the process.',
+
   kitchen: 'You are feeling kind of lazy. What should you eat?',
+
   pancakes: `The pancakes were deliciouse...look at the time! You don't want to be late!`,
+
   instantRamen: `You put your ramen in the microwave for 5 mins, but forget to put some water in it. The ramen catches fire and your microwave explodes launching you through your apartment wall. You land on your neighbor's cat killing him. Your neighbor, raged in anger, shoots you dead.`,
+
+  headOut: 'You hurry out your apartment shirtless, and walk to your car. Your skin shines bright in the sunlight due to all the moisturizer you have been using. A nearby driver is blinded by your radian skin. He looses control of his car and runs you over',
+
   bussStop: 'BussStop promt',
+
   bikeRide: 'Bikeride promt',
+
   run: 'Run prompt',
+
   death: 'You failed to get to work.'
 
   
@@ -48,6 +63,7 @@ var winner, state;
 
 /*----- cached element references -----*/ 
 const msgEl = document.getElementById('msg');
+const bodyEl = document.querySelector('body');
 const button1 = document.getElementById('button-one');
 const button2 = document.getElementById('button-two');
 //const buttons = document.getElementsByClassName('')
@@ -113,6 +129,10 @@ function handleBTwo(evt) {
       } else {
         if (state === story.kitchen) {
           state = story.instantRamen;
+        } else {
+          if (state === story.pancakes) {
+            state = story.headOut;
+          }
         }
       }
     }
@@ -127,75 +147,18 @@ function handleBTwo(evt) {
 function render() {
   msgEl.textContent = state;
   stateCheckingWithSwitch()
-  // if (state === story['Home']) {
-  //   button1.textContent = storyChoice['Home']['button-one'];
-  //   button2.textContent = storyChoice['Home']['button-two'];
-
-  // } else {
-  //   if (state === story['Wakeup']) {
-  //   button1.textContent = storyChoice['Wakeup']['button-one'];
-  //   button2.textContent = storyChoice['Wakeup']['button-two'];
-  //   } else {
-  //     if (state === story['Brakealarm']) {
-  //     button1.textContent = storyChoice['Brakealarm']['button-one'];
-  //     button2.textContent = storyChoice['Brakealarm']['button-two'];
-  //     } else {
-  //       if (state === story['Kitchen']) {
-  //         button1.textContent = storyChoice['Kitchen']['button-one'];
-  //         button2.textContent = storyChoice['Kitchen']['button-two'];
-  //       } else {
-  //         if (state === story['BussStop']) {
-  //           button1.textContent = storyChoice['BussStop']['button-one'];
-  //           button2.textContent = storyChoice['BussStop']['button-two'];
-  //         } else {
-  //           if (state === story['Bike ride']) {
-  //             button1.textContent = storyChoice['Bike ride']['button-one'];
-  //             button2.textContent = storyChoice['Bike ride']['button-two'];
-
-  //           } else {
-  //             if (state === story['Shower']) {
-  //               button1.textContent = storyChoice['Death']['button-one'];
-  //               button2.textContent = storyChoice['Death']['button-two'];
-  //             } else {
-  //               if (state === story['Tend Wound']) {
-  //                 button1.textContent = storyChoice['Death']['button-one'];
-  //                 button2.textContent = storyChoice['Death']['button-two'];
-  //               } else {
-  //                 if (state === story['Lick Wound']) {
-  //                   button1.textContent = storyChoice['Nocomment']['button-one'];
-  //                   button2.textContent = storyChoice['Nocomment']['button-two'];
-  //                 } else {
-  //                   if (state === story['Instant ramen']) {
-  //                     button1.textContent = storyChoice['Death']['button-one'];
-  //                     button2.textContent = storyChoice['Death']['button-two'];
-  //                   }
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   } 
-  // } 
- 
-  
-
-
-  
-  
 }
 
 
 function stateCheckingWithSwitch () {
   switch (state) {
     case story.home:
-       button1.textContent = storyChoice.home['button-one'];
-       button2.textContent = storyChoice.home['button-two'];
+      button1.textContent = storyChoice.home['button-one'];
+      button2.textContent = storyChoice.home['button-two'];
       break;
     case story.wakeup:
       button1.textContent = storyChoice.wakeup['button-one'];
-       button2.textContent = storyChoice.wakeup['button-two'];
+      button2.textContent = storyChoice.wakeup['button-two'];
       break;
     case story.brakeAlarm:
       button1.textContent = storyChoice.brakeAlarm['button-one'];
@@ -208,10 +171,14 @@ function stateCheckingWithSwitch () {
     case story.shower:
       button1.textContent = storyChoice.death['button-one'];
       button2.textContent = storyChoice.death['button-two'];
+      bodyEl.style.backgroundColor = '#8e160e';
+      bodyEl.style.color = 'black';
       break; 
     case story.tendWound:
       button1.textContent = storyChoice.death['button-one'];
-      button2.textContent = storyChoice.death['button-two']; 
+      button2.textContent = storyChoice.death['button-two'];
+      bodyEl.style.backgroundColor = '#8e160e';
+      bodyEl.style.color = 'black'; 
       break;
     case story.lickWound:
       button1.textContent = storyChoice.noComment['button-one'];
@@ -220,19 +187,25 @@ function stateCheckingWithSwitch () {
     case story.instantRamen:
       button1.textContent = storyChoice.death['button-one'];
       button2.textContent = storyChoice.death['button-two']; 
+      bodyEl.style.backgroundColor = '#8e160e';
+      bodyEl.style.color = 'black';
       break;
     case story.pancakes:
       button1.textContent = storyChoice.pancakes['button-one'];
       button2.textContent = storyChoice.pancakes['button-two']; 
-      break;  
+      break; 
+    case story.headOut:
+      button1.textContent = storyChoice.death['button-one'];
+      button2.textContent = storyChoice.death['button-two']; 
+      bodyEl.style.backgroundColor = '#8e160e';
+      bodyEl.style.color = 'black';
+      break;   
     default:
-  }
-  
+  } 
 }
 
 
 function init() {
-  state = story['standby'];
+  state = story.standby;
   render();
-
 }
